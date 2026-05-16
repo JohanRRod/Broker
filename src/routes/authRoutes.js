cae7b0070cade0636f8a2bf60461283741ffd408
JohanRRod <129108892+JohanRRod@users.noreply.github.com>
2026-05-16 15:07:18 -0600

    actualizacionnn

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, me } = require("../controllers/authController");
+const { register, login, me } = require("../controllers/authcontroller");
 const { verifyToken } = require("../middleware/auth");
 
 router.post("/auth/customers/register", register);
