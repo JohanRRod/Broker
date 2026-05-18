6a13520da097bb3d3fe9cb5c1ccae6ae1f84cb7c
JohanRRod <129108892+JohanRRod@users.noreply.github.com>
2026-05-17 20:32:19 -0600

    porfa funciona

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, me } = require("../controllers/authcontroller");
+const { register, login, me } = require("../controllers/authController");
 const { verifyToken } = require("../middleware/auth");
 
 router.post("/auth/customers/register", register);
